1b926b6bb77555e627e1c16a2b059b37279e6e8b
JeyBarrette <gilmetdinovaruslana@gmail.com>
2024-03-26 18:50:35 +0500

    lab23(half)

--- VetoshkinLanguage/VetoshkinLanguage/ClientPage.xaml.cs
+++ VetoshkinLanguage/VetoshkinLanguage/ClientPage.xaml.cs
@@ -20,11 +20,160 @@ namespace VetoshkinLanguage
     /// </summary>
     public partial class ClientPage : Page
     {
+        int CountInPage = 10;
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Client> CurrentPageList = new List<Client>();
+        List<Client> TableList;
+
         public ClientPage()
         {
             InitializeComponent();
+            List<Client> currentClients = VetoshkinLanguageEntities.GetContext().Client.ToList();
+            ClientListView.ItemsSource = currentClients;
+            ClientNumCombo.SelectedIndex = 0;
+            TBAllRecords.Text = VetoshkinLanguageEntities.GetContext().Client.Count().ToString();
+            UpdateClients();
+        }
+
+        private void UpdateClients()
+        {
             var currentClients = VetoshkinLanguageEntities.GetContext().Client.ToList();
             ClientListView.ItemsSource = currentClients;
+            TBAllRecords.Text = VetoshkinLanguageEntities.GetContext().Client.Count().ToString();
+            TBCount.Text = currentClients.Count().ToString();
+            TableList = currentClients;
+
+            if (ClientNumCombo.SelectedIndex == 0)
+            {
+                CountPage = 10;
+            }
+            else if (ClientNumCombo.SelectedIndex == 1)
+            {
+                CountPage = 50;
+            }
+            else if (ClientNumCombo.SelectedIndex == 2)
+            {
+                CountPage = 200;
+            }
+            else
+            {
+                CountPage = 0;
+            }
+
+            ChangePage(0, 0);
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+            if (CountInPage != 0)
+            {
+                if (CountRecords % CountInPage > 0)
+                {
+                    CountPage = CountRecords / CountInPage + 1;
+                }
+                else
+                {
+                    CountPage = CountRecords / CountInPage;
+                }
+
+                Boolean Ifupdate = true;
+
+                int min;
+                if (selectedPage.HasValue)
+                {
+                    if (selectedPage >= 0 && selectedPage <= CountPage)
+                    {
+                        CurrentPage = (int)selectedPage;
+                        min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                        for (int i = CurrentPage * CountInPage; i < min; i++)
+                        {
+                            CurrentPageList.Add(TableList[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    switch (direction)
+                    {
+                        case 1:
+                            if (CurrentPage > 0)
+                            {
+                                CurrentPage--;
+                                min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                                for (int i = CurrentPage * CountInPage; i < min; i++)
+                                {
+                                    CurrentPageList.Add(TableList[i]);
+                                }
+                            }
+                            else
+                            {
+                                Ifupdate = false;
+                            }
+                            break;
+                        case 2:
+                            if (CurrentPage < CountPage - 1)
+                            {
+                                CurrentPage++;
+                                min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                                for (int i = CurrentPage * CountInPage; i < min; i++)
+                                {
+                                    CurrentPageList.Add(TableList[i]);
+                                }
+                            }
+                            else
+                            {
+                                Ifupdate = false;
+                            }
+                            break;
+                    }
+                }
+                if (Ifupdate)
+                {
+                    PageListBox.Items.Clear();
+                    for (int i = 1; i <= CountPage; i++)
+                    {
+                        PageListBox.Items.Add(i);
+                    }
+                    PageListBox.SelectedIndex = CurrentPage;
+
+                    //min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CurrentPage;
+                    //TBCount.Text = min.ToString();
+                    TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                    ClientListView.ItemsSource = CurrentPageList;
+                    ClientListView.Items.Refresh();
+                }
+            }
+            else
+            {
+                PageListBox.Items.Clear();
+                PageListBox.Items.Add(1);
+            }
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+
+        private void ClientNumCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateClients();
         }
     }
 }
